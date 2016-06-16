95112ec7cd411cf7d491e7997b5dc9fd7dd0cb81
ClimbingWatermelon <774046169@qq.com>
2016-06-16 23:07:29 +0800

    login

---
+++ js/lxy/Popup.js
@@ -0,0 +1,12 @@
+﻿function showDiv() {
+    var winWidth;
+    var winHeight;
+    if (document.body && document.body.clientHeight && document.body.clientWidth) {
+        winHeight = document.body.clientHeight;
+        winWidth = document.body.clientWidth;
+    }
+    document.getElementById('show').style.display = "block";
+    document.getElementById('show_win').style.display = "block";
+    document.getElementById('show').style.width = winWidth + "px";
+    document.getElementById('show').style.height = winHeight + "px";
+}
